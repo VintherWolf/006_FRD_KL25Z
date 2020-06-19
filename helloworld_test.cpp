98b999556ca622b69cbdb011a9576d71337eae58
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-06-19 10:57:55 +0200

    Fix str in _test file

--- helloworld_test.cpp
+++ helloworld_test.cpp
@@ -2,12 +2,10 @@
 #include "catch.hpp"
 #include "helloworld.h"
 
-
-TEST_CASE ( "displayStringOnStdOut(char *str) is tested", "[5x hello world]" ) {
-
-std::strcpy(str, "Hello World");
-displayStringOnStdOut(str);
-REQUIRE( std::strcmp( str, "Hello World!" ) == 0 );
+TEST_CASE("displayStringOnStdOut(char *str) is tested", "[5x hello world]")
+{
+    char str[20];
+    std::strncpy(str, "Hello World", 20);
+    displayStringOnStdOut(str);
+    REQUIRE(std::strcmp(str, "Hello World!") == 0);
 }
-
-
